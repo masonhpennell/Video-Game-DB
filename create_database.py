da3d5152b74f88a30891644e01c30757e3697ffa
Teresa Wong <98933253+teresa812@users.noreply.github.com>
2024-12-11 18:23:31 -0800

    Create create_database.py
    add your password to password section

---
+++ create_database.py
@@ -0,0 +1,27 @@
+import mysql.connector
+import streamlit as st
+
+'''
+ONLY RUN THIS CODE ONCE
+CREATING THE DATABASE
+'''
+
+
+conn = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            password = '',
+            auth_plugin = 'mysql_native_password'
+        )
+
+cur_obj = conn.cursor()
+
+cur_obj.execute("CREATE SCHEMA VideoGameProject;")
+
+cur_obj.execute("SHOW DATABASES;")
+for row in cur_obj:
+        print(row)
+
+print(conn)
+
+conn.close()
