059b0280f131c9f32d37f4d8e6ebfe107a64613f
Teresa Wong <98933253+teresa812@users.noreply.github.com>
2024-12-15 16:05:18 -0800

    Update 3_Delete.py

--- pages/3_Delete.py
+++ pages/3_Delete.py
@@ -6,6 +6,16 @@ cursor = st.session_state['cursor']
 
 st.title("Delete a game")
 
+# Require login
+if "logged_in" not in st.session_state or not st.session_state.logged_in:
+    st.warning("Please log in to access this page.")
+    st.stop()
+
+conn = st.session_state['conn']
+cursor = st.session_state['cursor']
+
+st.title("Search for a game")
+
 cursor.execute('SELECT COUNT(*) FROM game;')
 max = cursor.fetchone()[0]
 id = st.number_input("Enter game ID", 0, max, 0, 1)
@@ -20,3 +30,4 @@ cursor.execute(f'SELECT gameID, title FROM game WHERE isDeleted = {deleted};')
 data = cursor.fetchall()
 df = pd.DataFrame(data, columns=cursor.column_names)
 st.table(df)
+
