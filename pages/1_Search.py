3c2e13d8b061f01fbd52cbe6a48b7acd93d482a0
Teresa Wong <98933253+teresa812@users.noreply.github.com>
2024-12-15 16:05:44 -0800

    Update 1_Search.py

--- pages/1_Search.py
+++ pages/1_Search.py
@@ -1,11 +1,22 @@
 import streamlit as st
 import pandas as pd
 
+conn = st.session_state['conn']
 cursor = st.session_state['cursor']
 
 st.title("Search for a game")
 
 title = st.text_input("Game Title")
+
+# Require login
+if "logged_in" not in st.session_state or not st.session_state.logged_in:
+    st.warning("Please log in to access this page.")
+    st.stop()
+
+conn = st.session_state['conn']
+cursor = st.session_state['cursor']
+
+st.title("Search for a game")
 
 cursor.execute("SELECT name FROM developer;")
 developers = [d for (d,) in cursor.fetchall()]
@@ -31,3 +42,6 @@ cursor.execute(f'''
 data = cursor.fetchall()
 df = pd.DataFrame(data, columns=cursor.column_names)
 st.table(df)
+
+
+
