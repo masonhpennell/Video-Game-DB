ec7ab73e05e9c1e445abc12a9742f59f81ba70aa
Teresa Wong <98933253+teresa812@users.noreply.github.com>
2024-12-15 16:04:52 -0800

    Update 5_Export.py

--- pages/5_Export.py
+++ pages/5_Export.py
@@ -2,6 +2,17 @@ import streamlit as st
 import os
 
 st.title("Download Data")
+
+# Require login
+if "logged_in" not in st.session_state or not st.session_state.logged_in:
+    st.warning("Please log in to access this page.")
+    st.stop()
+
+conn = st.session_state['conn']
+cursor = st.session_state['cursor']
+
+st.title("Search for a game")
+
 st.subheader("Select the data you want to download")
 
 # assign directory
@@ -13,3 +24,4 @@ for root, dirs, files in os.walk(directory):
     for filename in files:
         st.download_button(label=filename.replace('.csv',''),
             data=os.path.join(root, filename), file_name=filename)
+        
